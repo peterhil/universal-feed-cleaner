ed25badb26af6373ffe38b288c5e808a3881a908
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-01-23 10:45:32 +0200

    Log XmlHttpRequests on background script

--- src/js/background/script.js
+++ src/js/background/script.js
@@ -1,1 +1,14 @@
 console.log('[UFC]: background script')
+
+function onRequestCompleted (details) {
+    if (details.method === "GET" && details.type === "xmlhttprequest") {
+        console.info('XHR completed:', details)
+    }
+
+    return { cancel: false }
+}
+
+browser.webRequest.onCompleted.addListener(
+    onRequestCompleted,
+    { urls: ['<all_urls>'] },
+)
