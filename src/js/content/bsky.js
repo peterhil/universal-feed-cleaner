9eae7a916791416d6d7002fa0dbc94bbc9ac5e1b
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-01-23 10:07:08 +0200

    Find vertical containers

--- src/js/content/bsky.js
+++ src/js/content/bsky.js
@@ -1,3 +1,19 @@
-export function main () {
-    console.log('[UFC] bsky.js on', document.location.href)
+function isVerticalContainer (elem) {
+    return elem.childElementCount > 4 && elem.offsetHeight > elem.offsetWidth
 }
+
+function findContainers () {
+    return Array.from(document.querySelectorAll('*')).filter(isVerticalContainer)
+}
+
+export function main () {
+    const containers = findContainers()
+    console.log('[UFC] main: bsky.js on', document.location.href, containers)
+}
+
+console.log('[UFC]: loaded on', document.location.href)
+
+window.addEventListener('load', main, false)
+// document.addEventListener("DOMContentLoaded", main)
+
+setTimeout(main, 7000)
