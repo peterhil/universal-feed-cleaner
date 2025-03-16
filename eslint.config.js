4012c7dea40bfd86b9fb6f314190301b51f37b55
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-16 19:04:38 +0200

    deps: eslint: Fix the new flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,65 @@
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import globals from "globals"
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
 
-import importPlugin from 'eslint-plugin-import'
-import n from 'eslint-plugin-n'
-import promise from 'eslint-plugin-promise'
+import importPlugin from "eslint-plugin-import"
+import n from "eslint-plugin-n"
+import promise from "eslint-plugin-promise"
 
-import { defineConfig } from 'eslint/config'
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript"
 
-export default defineConfig([
-    {
-        files: [
-            "**/*.{js,mjs,cjs,ts}"
-        ],
-        languageOptions: {
-            parserOptions: {
-                ecmaFeatures: {
-                    impliedstrict: true,
-                },
-            },
+const languageOptions = {
+    parserOptions: {
+        ecmaVersion: 'latest',  // or a year
+        ecmaFeatures: {
+            impliedstrict: true,
         },
-        "plugins": {
-            importPlugin,
-            n,
-            promise,
+        globals: {
+            ...globals.browser,
         },
-        rules: {
-            'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
-            'comma-dangle': ['off', 'always'],
-            indent: ['error', 4],
-            'no-console': ['off', 'always'],
-            // 'no-console': ['warn', {}],
-            'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
-        },
+        sourceType: "module",
     },
-    pluginJs.configs.recommended,
-    importPlugin.flatConfigs.recommended,
-    ...tseslint.configs.recommended,
+}
+
+const plugins = {
+    importPlugin,
+    n,
+    promise,
+}
+
+const rules = {
+    "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+    "comma-dangle": ["off", "always"],
+    "indent": ["error", 4],
+    "no-console": ["off", "always"],
+    // "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-multiple-empty-lines": ["error", { max: 1, maxBOF: 0, maxEOF: 0 }],
+}
+
+export default tseslint.config([
+    js.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        languageOptions,
+        plugins,
+        rules,
+    },
+    {
+        files: ["**/*.{ts,tsx}"],
+        extends: [
+            importPlugin.flatConfigs.recommended,
+            importPlugin.flatConfigs.typescript,
+        ],
+        languageOptions,
+        plugins,
+        rules,
+        settings: {
+            "import-x/resolver-next": [
+                createTypeScriptImportResolver({
+                    alwaysTryTypes: true,
+                    project: "./tsconfig.json",
+                }),
+            ],
+        },
+    }
 ])
