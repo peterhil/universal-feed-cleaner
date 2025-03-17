b73d9394e629eea269ce880b6e853dd2b1ed68e4
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-17 14:54:33 +0200

    Add keywords textarea to options

--- src/lib/component/Options.svelte
+++ src/lib/component/Options.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+    import { options } from '$lib/options'
 
+    let keywords: string[] = [
+        'Alpha',
+        'Beta'
+    ]
 </script>
 
-<h1>Options</h1>
+<div>
+    <h1>Options</h1>
+
+    <h2>Keywords</h2>
+
+    <textarea
+        placeholder="Add keywords, one per line"
+        rows="12"
+        bind:value={keywords}
+        >
+    </textarea>
+</div>
