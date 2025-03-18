8d938d76610e5267390aec4e643a403e468a2b43
Peter Hillerström <peter.hillerstrom@gmail.com>
2025-03-18 09:00:48 +0200

    Clean up the popup UI

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-    import svelteLogo from '../../assets/svelte.svg'
-    import Counter from '../../lib/Counter.svelte'
-
     function openOptionsPage () {
         browser.runtime.openOptionsPage()
         return false
@@ -9,14 +6,6 @@
 </script>
 
 <main>
-    <div>
-        <a href="https://wxt.dev" target="_blank" rel="noreferrer">
-            <img src="/wxt.svg" class="logo" alt="WXT Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-        </a>
-    </div>
     <h1>
         Universal<br>
         feed cleaner
@@ -26,31 +15,19 @@
         <button type="button" on:click={openOptionsPage}>
             Options
         </button>
-        <Counter />
     </div>
 
-    <p class="read-the-docs">
+    <p class="help">
         Source code and instructions on
-        <a href="https://github.com/peterhil/universal-feed-cleaner/">
+        <a href="https://github.com/peterhil/universal-feed-cleaner/"
+           target="_blank" rel="noreferrer">
             Github
         </a>
     </p>
 </main>
 
 <style>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-    }
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #54bc4ae0);
-    }
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-    .read-the-docs {
+    .help {
         color: #888;
     }
 </style>
